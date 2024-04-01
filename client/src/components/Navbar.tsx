00e62f5140f8ad9a8e55ac99c9fc9e01b14cd015
Adam Chhor <chhoradam@gmail.com>
2024-04-01 14:50:37 -0700

    made nav bar semi responsive

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,13 +7,32 @@ import {
   Button,
   Avatar,
   Box,
+  Menu,
+  MenuItem,
+  Container,
 } from '@mui/material';
 import beachPartyLogo from '../assets/beach-party-logo.svg';
+import MapsUgcIcon from '@mui/icons-material/MapsUgc';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import AddIcon from '@mui/icons-material/Add';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 import React from 'react';
 import { UserAuth } from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+import { TravelExplore } from '@mui/icons-material';
 const Navbar: React.FC = () => {
   const { user, logOut } = UserAuth();
+  console.count();
+  const [anchorProfileElement, setAnchorProfileElement] =
+    React.useState<null | HTMLElement>(null);
+
+  const profileMenuHandler = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorProfileElement(event.currentTarget);
+  };
+
+  const closeProfileMenuHandler = () => {
+    setAnchorProfileElement(null);
+  };
 
   const signOutHandler = async () => {
     try {
@@ -33,7 +52,7 @@ const Navbar: React.FC = () => {
         }}>
         <Toolbar>
           <Button
-            href='/home'
+            href={user ? '/home' : '/'}
             color='inherit'
             sx={{ minHeight: 0, minWidth: 0, padding: 0 }}>
             {/* <img src={beachPartyLogo} alt='' /> */}
@@ -51,37 +70,86 @@ const Navbar: React.FC = () => {
               Beach Party
             </Typography> */}
           </Button>
-          <Stack direction='row' spacing={2}>
-            <Button href='/find-event' variant='contained' color='secondary'>
-              Find Event
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: 'flex',
+              gap: '1rem',
+            }}>
+            <Button
+              href='/find-event'
+              variant='contained'
+              color='secondary'
+              sx={{ my: 2 }}
+              endIcon={<TravelExplore />}>
+              <Box
+                sx={{
+                  display: { xs: 'none', sm: 'block' },
+                  maxHeight: '150px',
+                }}>
+                Find Event
+              </Box>
+              {/* <Typography variant='body2'>Find Event</Typography> */}
             </Button>
+            {user && (
+              <Button
+                href='/create-event'
+                variant='contained'
+                color='secondary'
+                sx={{ my: 2 }}
+                endIcon={<AddLocationAltIcon />}>
+                <Box
+                  sx={{
+                    maxHeight: '150px',
+                    display: { xs: 'none', sm: 'block' },
+                  }}>
+                  Create Event
+                </Box>
+              </Button>
+            )}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
             {user ? (
               <>
-                <Button
-                  href='/create-event'
-                  variant='contained'
-                  color='secondary'>
-                  Create Event
-                </Button>
-                <Button variant='contained' onClick={signOutHandler}>
+                {/* <Button variant='contained' onClick={signOutHandler}>
                   Log Out
+                </Button> */}
+
+                <Button onClick={profileMenuHandler} color='inherit'>
+                  <>
+                    <Typography
+                      variant='body1'
+                      component='h2'
+                      sx={{ flexGrow: 1 }}>
+                      {user.displayName.substring(0, 25)}
+                    </Typography>
+                    <Avatar alt={user.displayName} src={user.photoURL} />
+                  </>
                 </Button>
-                <Button href='/profile/' color='inherit'>
-                  <Typography
-                    variant='body1'
-                    component='h2'
-                    sx={{ flexGrow: 1 }}>
-                    {user.displayName}
-                  </Typography>
-                  <Avatar alt={user.displayName} src={user.photoURL} />
-                </Button>
+                <Menu
+                  anchorEl={anchorProfileElement}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorProfileElement)}
+                  onClose={closeProfileMenuHandler}>
+                  <MenuItem onClick={logOut}>
+                    <Typography textAlign='center'>Log out</Typography>
+                  </MenuItem>
+                </Menu>
               </>
             ) : (
               <Button href='/log-in' variant='contained' color='secondary'>
                 Log In
               </Button>
             )}
-          </Stack>
+          </Box>
         </Toolbar>
       </AppBar>
     </>
